36a91a9db86342028929ed19538d30d18f537ef0
watopoly246 <109627176+watopoly246@users.noreply.github.com>
2022-07-20 16:59:17 -0400

    Add files via upload

---
+++ display.h
@@ -0,0 +1,16 @@
+#ifndef _DISPLAY_H
+#define _DISPLAY_H
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class Display {
+
+public:
+    void printDisplay();
+};
+
+
+#endif
