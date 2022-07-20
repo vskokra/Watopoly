4bcc62ead26178d83e758b05bf5c7dcf0bd98aa0
watopoly246 <109627176+watopoly246@users.noreply.github.com>
2022-07-20 09:27:49 -0400

    Add files via upload

--- player.h
+++ player.h
@@ -5,13 +5,20 @@ class Player{
     char playerChar;
     int position;
     int money;
+    bool Tims;
+    int rollsTims;
 
 public:
 
     int getPosition(); // gives players position
-    void setPosition(int sum); //sets new position
+    void setPosition(int pos); //sets new position
     int getMoney(); // gives info of money
     void money_add(unsigned int val);
     void money_sub(unsigned int val);
+    void goToTims();
+    void updateRollsTims(int n);
+    bool isBankrupt();
+    void updateWorth();
+    void declareBankrupt();
 
 };
