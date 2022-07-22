5d69e3dd6e58d18f60e601ab69a95cf8c23ebacf
watopoly246 <109627176+watopoly246@users.noreply.github.com>
2022-07-22 17:22:49 -0400

    Add files via upload

--- project2/nonOwnable.cc
+++ project2/nonOwnable.cc
@@ -1,152 +1,162 @@
 #include "nonOwnable.h"
 using namespace std;
-void GoToTims::function(Player *curr){
-    curr->goToTims();
-}
 
-void GooseNesting::function(Player *curr){}
+NonOwnable::NonOwnable(int position, GameBoard *g): pos{position}, game{g}{} 
 
-void Tuition::function(Player *curr){
+void NonOwnable::doOperation(){
+    if(pos == 30){//GO TO TIMS
+        game->currPlayer->setPosition(10);
+        game->currPlayer->goToTims(true);
+    }
+   if(pos == 20){} //Goose Nesting
+   if(pos == 4){ //Tuition
     cout << "Press 1 to pay $300 or Press 2 to pay 10%% of your total worth" << endl;
     int i;
     while (true) {
         cin >> i;
         if (cin) {
             if(i == 1){
-                while (curr->getMoney() < 300){
-                    if (curr->isBankrupt()){
-                    curr->declareBankrupt();
+                while (game->currPlayer->getMoney() < 300){
+                    if (game->currPlayer->isBankrupt(300)){
+                    game->currPlayer->declareBankrupt();
                     }
                     else{
                         cout<< "You don't have enough money! You have the following choices: bankrupt, trade, mortgage, improve sell"<<endl;  
                     }
                 }
-                curr->money_sub(300);
+                game->currPlayer->money_sub(300);
                 break;
             }
             else if (i == 2){
-                int total = (curr->getMoney() + curr->getWorth())*0.1;
-                 while (curr->getMoney() < total){
-                    if (curr->isBankrupt()){
-                    curr->declareBankrupt();
+                int total = (game->currPlayer->getMoney() + game->currPlayer->getWorth())*0.1;
+                 while (game->currPlayer->getMoney() < total){
+                    if (game->currPlayer->isBankrupt(total)){
+                    game->currPlayer->declareBankrupt();
                     }
                     else{
                         cout<< "You don't have enough money! You have the following choices: bankrupt, trade, mortgage, improve sell"<<endl;  
                     }
                 }
-                curr->money_sub(total);
+                game->currPlayer->money_sub(total);
                 break;
 
             }
         }
     }
-}
 
-void CoopFee::function(Player *curr){
-    while (curr->getMoney() < 150){
-        if (curr->isBankrupt()){
-            curr->declareBankrupt();
+   }
+   if(pos == 2 || pos == 17 || pos == 33){//SLC
+    srand(time(NULL));
+	int rimProb = rand()%100;
+	if(rimProb == 0){
+        if(game->rimCupCount < 4){
+            game->currPlayer->setRimCups(game->currPlayer->getRimCups() + 1);
+        }
+    }
+    else {
+	int shuffle = rand()%24 + 1;
+
+	if(shuffle >= 1 && shuffle <= 3){
+        int oldPosition = game->currPlayer->getPosition();
+        int sum = oldPosition - 3;
+        game->currPlayer->setPosition(sum);
+	} else if (shuffle >= 4 && shuffle <= 7){
+        int oldPosition = game->currPlayer->getPosition();
+        int sum = oldPosition - 2;
+        game->currPlayer->setPosition(sum);
+	} else if (shuffle >= 8 && shuffle <= 11){
+        int oldPosition = game->currPlayer->getPosition();
+        int sum = oldPosition - 1;
+        game->currPlayer->setPosition(sum);
+	} else if (shuffle >= 12 && shuffle <= 14){
+        int oldPosition = game->currPlayer->getPosition();
+        int sum = oldPosition + 1;
+        game->currPlayer->setPosition(sum);
+	} else if (shuffle >= 15 && shuffle <= 18){
+        int oldPosition = game->currPlayer->getPosition();
+        int sum = oldPosition + 2;
+        game->currPlayer->setPosition(sum);
+	} else if (shuffle >= 19 && shuffle <= 22){
+        int oldPosition = game->currPlayer->getPosition();
+        int sum = oldPosition + 3;
+        game->currPlayer->setPosition(sum);
+	} else if (shuffle = 23){
+        int oldPosition = game->currPlayer->getPosition();
+        int sum = 10;
+        game->currPlayer->setPosition(sum);
+	} else {
+        int oldPosition = game->currPlayer->getPosition();
+        int sum = 0;
+        game->currPlayer->setPosition(sum);
+        game->currPlayer->money_add(200);
+    }
+    }
+   }
+
+    if(pos == 7 || pos == 22 || pos == 36){
+    srand(time(NULL));
+	int rimProb = rand()%100;
+    if(rimProb == 0){
+        if(game->rimCupCount < 4){
+            game->currPlayer->setRimCups(game->currPlayer->getRimCups() + 1);
+        }
+    }
+    else{
+	int shuffle = rand()%18 + 1;
+
+	if(shuffle = 1){
+        while (game->currPlayer->getMoney() < 200){
+            if (game->currPlayer->isBankrupt(200)){
+                game->currPlayer->declareBankrupt();
+                }
+            else{
+                cout<< "You don't have enough money! You have the following choices: bankrupt, trade, mortgage, improve sell"<<endl;  
+                }
+            }
+            game->currPlayer->money_sub(200); 
+	} else if (shuffle >= 2 && shuffle <= 3){
+        while (game->currPlayer->getMoney() < 100){
+            if (game->currPlayer->isBankrupt(100)){
+                game->currPlayer->declareBankrupt();
+                }
+            else{
+                cout<< "You don't have enough money! You have the following choices: bankrupt, trade, mortgage, improve sell"<<endl;  
+                }
+            }
+            game->currPlayer->money_sub(100); 
+	} else if (shuffle >= 4 && shuffle <= 6){
+        while (game->currPlayer->getMoney() < 50){
+            if (game->currPlayer->isBankrupt(50)){
+                game->currPlayer->declareBankrupt();
+                }
+            else{
+                cout<< "You don't have enough money! You have the following choices: bankrupt, trade, mortgage, improve sell"<<endl;  
+                }
+            }
+            game->currPlayer->money_sub(50); 
+	} else if (shuffle >= 7 && shuffle <= 12){
+        game->currPlayer->money_add(25);
+	} else if (shuffle >= 13 && shuffle <= 15){
+        game->currPlayer->money_add(50);
+	} else if (shuffle >= 16 && shuffle <= 17){
+        game->currPlayer->money_add(100);
+	} else if (shuffle = 18){
+        game->currPlayer->money_add(200);
+	}
+}
+    }
+
+    if(pos == 38){
+    while (game->currPlayer->getMoney() < 150){
+        if (game->currPlayer->isBankrupt(150)){
+            game->currPlayer->declareBankrupt();
             }
         else{
             cout<< "You don't have enough money! You have the following choices: bankrupt, trade, mortgage, improve sell"<<endl;  
             }
     }
-    curr->money_sub(150);
+    game->currPlayer->money_sub(150);
 }
-
-void SLC::function(Player *curr){
-    srand(time(NULL));
-	int rimProb = rand()%100;
-	if(rimProb == 0){
-        // Get Cup
-		}
-	int shuffle = rand()%24 + 1;
-
-	if(shuffle >= 1 && shuffle <= 3){
-        int oldPosition = curr->getPosition();
-        int sum = oldPosition - 3;
-        curr->setPosition(sum);
-	} else if (shuffle >= 4 && shuffle <= 7){
-        int oldPosition = curr->getPosition();
-        int sum = oldPosition - 2;
-        curr->setPosition(sum);
-	} else if (shuffle >= 8 && shuffle <= 11){
-        int oldPosition = curr->getPosition();
-        int sum = oldPosition - 1;
-        curr->setPosition(sum);
-	} else if (shuffle >= 12 && shuffle <= 14){
-        int oldPosition = curr->getPosition();
-        int sum = oldPosition + 1;
-        curr->setPosition(sum);
-	} else if (shuffle >= 15 && shuffle <= 18){
-        int oldPosition = curr->getPosition();
-        int sum = oldPosition + 2;
-        curr->setPosition(sum);
-	} else if (shuffle >= 19 && shuffle <= 22){
-        int oldPosition = curr->getPosition();
-        int sum = oldPosition + 3;
-        curr->setPosition(sum);
-	} else if (shuffle = 23){
-        int oldPosition = curr->getPosition();
-        int sum = 10;
-        curr->setPosition(sum);
-	} else {
-        int oldPosition = curr->getPosition();
-        int sum = 0;
-        curr->setPosition(sum);
-        curr->money_add(200);
-    }
 }
-
-void NeedlesHall::function(Player *curr){
-    srand(time(NULL));
-	int rimProb = rand()%100;
-	if(rimProb == 0){
-        // Get Cup
-		}
-	int shuffle = rand()%18 + 1;
-
-	if(shuffle = 1){
-        while (curr->getMoney() < 200){
-            if (curr->isBankrupt()){
-                curr->declareBankrupt();
-                }
-            else{
-                cout<< "You don't have enough money! You have the following choices: bankrupt, trade, mortgage, improve sell"<<endl;  
-                }
-            }
-            curr->money_sub(200); 
-	} else if (shuffle >= 2 && shuffle <= 3){
-        while (curr->getMoney() < 100){
-            if (curr->isBankrupt()){
-                curr->declareBankrupt();
-                }
-            else{
-                cout<< "You don't have enough money! You have the following choices: bankrupt, trade, mortgage, improve sell"<<endl;  
-                }
-            }
-            curr->money_sub(100); 
-	} else if (shuffle >= 4 && shuffle <= 6){
-        while (curr->getMoney() < 50){
-            if (curr->isBankrupt()){
-                curr->declareBankrupt();
-                }
-            else{
-                cout<< "You don't have enough money! You have the following choices: bankrupt, trade, mortgage, improve sell"<<endl;  
-                }
-            }
-            curr->money_sub(50); 
-	} else if (shuffle >= 7 && shuffle <= 12){
-        curr->money_add(25);
-	} else if (shuffle >= 13 && shuffle <= 15){
-        curr->money_add(50);
-	} else if (shuffle >= 16 && shuffle <= 17){
-        curr->money_add(100);
-	} else if (shuffle = 18){
-        curr->money_add(200);
-	}
-}
-
-
 
 
