0e380ced74d889f2929127b649ce9d396000e789
watopoly246 <109627176+watopoly246@users.noreply.github.com>
2022-07-23 13:10:48 -0400

    Update ownable.h

--- ownable.h
+++ ownable.h
@@ -4,13 +4,17 @@
 #include <string>
 #include "boardcell.h"
 #include "player.h"
+#include "gameboard.h"
+
 class Ownable: public BoardCell{
     int cost;
     bool isMortgage = false; //default val
+    GameBoard *game;
     public:
+        void buyProperty();
         virtual void doOperation(Player * curr) override = 0; 
         virtual int getImprovs() override; 
-        Ownable(std::string name, int cost, Player *owner);
+        Ownable(std::string name, int cost, Player *owner, GameBoard *game);
         std::string propName;
         Player *owner; // set up owner field
 }; 
