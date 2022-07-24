f12a3f3187fe8781ab44339a7b7afd1095758792
watopoly246 <109627176+watopoly246@users.noreply.github.com>
2022-07-24 16:51:27 -0400

    Update ownable.cc

--- ownable.cc
+++ ownable.cc
@@ -6,9 +6,16 @@ using namespace std;
 
 
 //ctor 
-Ownable::Ownable(string name, int cost, Player *owner, GameBoard *game): propName(name), cost(cost), owner(owner), game{game}{}
+Ownable::Ownable(string name, int cost,  shared_ptr<Player>owner, GameBoard *game): propName(name), cost(cost), owner(owner), game{game}{}
 
 void Ownable::buyProperty(){
+    game->currPlayer->money_sub(cost);
     owner = game->currPlayer;
-    //update in player class
+    //int idx = game->currPlayer->getPosition();
+    //shared_ptr <Ownable> newProp = dynamic_pointer_cast <Ownable> (game->gb[idx]);
+    game->currPlayer->addProps(game->getOwnable());
+    game->currPlayer->updateWorth(cost);
+}
+
+void Ownable::auction(){
 }
