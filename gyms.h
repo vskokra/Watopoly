71aac5bf20eea2ca7a4ab24d5d214adad247188d
watopoly246 <109627176+watopoly246@users.noreply.github.com>
2022-07-24 21:35:41 -0400

    Update gyms.h

--- gyms.h
+++ gyms.h
@@ -1,6 +1,6 @@
 #ifndef _GYMS_H_
 #define _GYMS_H_
-
+#include <memory>
 #include "ownable.h"
 #include "gameboard.h"
 #include "dice.h"
@@ -9,10 +9,9 @@ class Gym : public Ownable{
     int calcRent(); 
     GameBoard *game;
     public:
-        Gym(std::string name, int cost, Player *owner, GameBoard *game);
-        void doOperation(shared_ptr <Player> curr) override;
+        Gym(std::string name, int cost, std::shared_ptr<Player>owner, GameBoard *game);
+        void doOperation(std::shared_ptr<Player>curr) override;
 
 };
 
 #endif
-
